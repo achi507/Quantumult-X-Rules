269f0da88aff89e2afe531c03e7011dfdfa480c2
achi507 <achi507@gmail.com>
2020-03-20 11:05:03 +0800

    Update

---
+++ JS/App Pricer.js
@@ -0,0 +1,56 @@
+/*
+App Pricer for Surge by Neurogram
+
+ - App 价格监控
+ 
+使用说明：https://www.notion.so/neurogram/App-Pricer-bb7f6877caa24e8584cc63bddf6e555a
+脚本地址:https://github.com/Neurogram-R/Surge/blob/master/AppPricer.js
+
+关于作者
+Telegram: Neurogram
+GitHub: Neurogram-R
+*/
+
+const region = "hk"
+const appIds = ["1482985563", "1373567447", "932747118", "1442620678" ,"1442620678","1488616799"]
+
+var cacheData = $persistentStore.read()
+if (!cacheData) {
+    cacheData = {}
+} else {
+    cacheData = JSON.parse(cacheData)
+}
+
+$httpClient.post('https://itunes.apple.com/lookup?id=' + appIds + "&country=" + region, function (error, response, data) {
+    if (error) {
+        console.log(error);
+        $notification.post("App Pricer", "获取价格失败")
+        $done()
+    } else {
+        let appData = JSON.parse(data).results
+        let priceChanged = ""
+        let newAppAdded = ""
+        for (var i = 0; i < appData.length; i++) {
+            if (cacheData[appData[i].trackId]) {
+                if (appData[i].formattedPrice != cacheData[appData[i].trackId].price) {
+                    priceChanged = priceChanged + "🏷 " + appData[i].trackName + "  " + cacheData[appData[i].trackId].price + " → " + appData[i].formattedPrice + "\n"
+                    cacheData[appData[i].trackId].price = appData[i].formattedPrice
+                }
+            } else {
+                newAppAdded = newAppAdded + "🏷 " + appData[i].trackName + "  " + appData[i].formattedPrice + "\n"
+                cacheData[appData[i].trackId] = {
+                    name: appData[i].trackName,
+                    price: appData[i].formattedPrice
+                }
+            }
+        }
+        if (priceChanged) {
+            $notification.post("Price Changed", "", priceChanged)
+        }
+        if (newAppAdded) {
+            $notification.post("New Apps Added", "", newAppAdded)
+        }
+        $persistentStore.write(JSON.stringify(cacheData))
+        $done()
+    }
+})
